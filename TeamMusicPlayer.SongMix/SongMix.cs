449769734fd7e338a64cc4fab3e5a1f90aa0c6dc
shwmyth <46229393+shwmyth@users.noreply.github.com>
2019-01-06 19:18:47 -0500

    Add files via upload

---
+++ TeamMusicPlayer.SongMix/SongMix.cs
@@ -0,0 +1,120 @@
+﻿namespace TeamMusicPlayer.SongMix
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using SongLibrary;
+    using Team;
+
+    public sealed class SongMix
+    {
+        private readonly HashSet<TeamMember> listeners = new HashSet<TeamMember>();
+        private readonly List<SongMixEntry> songMixEntries = new List<SongMixEntry>();
+
+        public SongMix(Team team)
+        {
+            Team = team;
+            addAllAvailableSongsToMix();
+        }
+
+        public Team Team { get; }
+
+        public SongMixEntry DetermineNextSongMixEntryToPlay()
+        {
+            if (songMixEntries.Count > 0)
+            {
+
+                IOrderedEnumerable<SongMixEntry> songMixEntriesInLastPlayedOrder =
+                    songMixEntries.OrderBy(s => s.LastPlayed);
+
+                SongMixEntry firstSongNotDislikedByACurrentListener =
+                    songMixEntriesInLastPlayedOrder.FirstOrDefault(s => s.IsNotDislikedByAnyListener(listeners));
+                return firstSongNotDislikedByACurrentListener;
+            }
+
+            return null;
+        }
+
+        public void AddListener(TeamMember teamMember)
+        {
+            listeners.Add(teamMember);
+        }
+
+        private void addAllAvailableSongsToMix()
+        {
+            IEnumerable<Song> allSongs = Songs.GetAllSongs();
+            foreach (Song song in allSongs)
+            {
+                var songMixEntry = new SongMixEntry(song);
+                songMixEntries.Add(songMixEntry);
+            }
+        }
+
+        public void addLikedSongsToMix()
+        {
+            IEnumerable<Song> allSongs = Songs.GetAllSongs();
+            foreach (Song song in allSongs)
+            {
+                var songMixEntry = new SongMixEntry(song);
+                int count = songMixEntry.GetLikesForSong(Team);
+
+                for (var i = 0; i < count; ++i)
+                {
+                    songMixEntries.Add(songMixEntry);
+
+                }
+            }
+        }
+
+        public int TotalEntryCount()
+        {
+            return songMixEntries.Count;
+        }
+
+        public int TotalEntryMinusDislikesCount()
+        {
+            if (songMixEntries.Count > 0)
+            {
+                IEnumerable<SongMixEntry> songMixEntriesMinusDislikes =
+                    songMixEntries.Where(s => s.IsNotDislikedByAnyListener(listeners));
+
+                return songMixEntriesMinusDislikes.Count();
+            }
+            else
+            {
+                return 0;
+            }
+            
+        }
+
+        public int SongInMixCount(string song)
+        {
+            if (songMixEntries.Count > 0)
+            {
+                IEnumerable<SongMixEntry> songMixEntriesForSong =
+                    songMixEntries.Where(s => s.Song.Name == song);
+
+                return songMixEntriesForSong.Count();
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        public string SongMostInMix()
+        {
+            if (songMixEntries.Count > 0)
+            {
+                string songMixEntriesForSong =
+                    songMixEntries.GroupBy(s => s.Song.Name).OrderByDescending(g => g.Count()).First().Key; 
+                return songMixEntriesForSong;
+            }
+            else
+            {
+                return "";
+            }
+
+        }
+    }
+}
---
+++ TeamMusicPlayer.SongMix/SongMixEntry.cs
@@ -0,0 +1,70 @@
+﻿namespace TeamMusicPlayer.SongMix
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using SongLibrary;
+    using SongRating;
+    using Team;
+
+    public sealed class SongMixEntry
+    {
+        private readonly SongRatingSet songRatingSet;
+
+        internal SongMixEntry(Song song)
+        {
+            Song = song;
+            songRatingSet = SongRatings.GetSongRatingSet(song);
+        }
+
+        public Song Song { get; }
+
+        public int PlayCount { get; private set; }
+
+        public DateTime LastPlayed { get; private set; } = DateTime.MinValue;
+
+        public void Play()
+        {
+            ++PlayCount;
+            LastPlayed = DateTime.Now;
+            Thread.Sleep(1); // to ensure LastPlayed timestamps are unique
+        }
+
+        public bool IsNotDislikedByAnyListener(IEnumerable<TeamMember> listeners)
+        {
+            foreach (TeamMember teamMember in listeners)
+            {
+                if (songRatingSet.HasDislikedRatingBy(teamMember))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public int GetLikesForSong(Team localteam)
+        {
+            int count = 0;
+
+            foreach (TeamMember teamMember in localteam.Members)
+            {
+                if (songRatingSet.HasLikedRatingBy(teamMember))
+                {
+                    count++;
+                }
+            }
+
+
+            //foreach (value in localsongRatingSet.songRatingsByTeamMemberId.Values)
+            //{
+            //    if (entry.Value.ToString() == "Like")
+            //    {
+            //        count++;
+            //    }
+            //}
+
+            return count;
+        }
+    }
+}
